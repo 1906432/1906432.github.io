69eba17e52dcc0ccf20bfcf3e39e7969245ff0d6
1906432 <87557225+1906432@users.noreply.github.com>
2021-07-24 20:44:00 +0900

    期末課題

--- final.js
+++ final.js
@@ -1,31 +1,35 @@
-function check1(){
-    if (document.fo1.anser.value=="猫"||document.fo1.anser.value=="ねこ"||document.fo1.anser.value=="ネコ"){
+function check1() {
+    if (document.fo1.anser.value == "猫" || document.fo1.anser.value == "ねこ" || document.fo1.anser.value == "ネコ") {
         alert("正解！🐏");
-    }else {
+    } else {
         alert("違います🐎");
     }
-}function check2(){
-    if (document.fo2.anser.value=="うぐいす"||document.fo2.anser.value=="ウグイス"||document.fo2.anser.value=="鶯"){
+}
+function check2() {
+    if (document.fo2.anser.value == "うぐいす" || document.fo2.anser.value == "ウグイス" || document.fo2.anser.value == "鶯") {
         alert("正解！🐏");
-    }else {
+    } else {
         alert("違います🐎");
     }
-}function check3(){
-    if (document.fo3.anser.value=="らいおん"||document.fo3.anser.value=="ライオン"||document.fo3.anser.value=="獅子"){
+}
+function check3() {
+    if (document.fo3.anser.value == "らいおん" || document.fo3.anser.value == "ライオン" || document.fo3.anser.value == "獅子") {
         alert("正解！🐏");
-    }else {
+    } else {
         alert("違います🐎");
     }
-}function check4(){
-    if (document.fo4.anser.value=="うみねこ"||document.fo4.anser.value=="ウミネコ"||document.fo4.anser.value=="海猫"){
+}
+function check4() {
+    if (document.fo4.anser.value == "うみねこ" || document.fo4.anser.value == "ウミネコ" || document.fo4.anser.value == "海猫") {
         alert("正解！🐏");
-    }else {
+    } else {
         alert("違います🐎");
     }
-}function check5(){
-    if (document.fo5.anser.value=="クジャク"||document.fo5.anser.value=="くじゃく"||document.fo5.anser.value=="孔雀"||document.fo5.anser.value=="インドクジャク"){
+}
+function check5() {
+    if (document.fo5.anser.value == "クジャク" || document.fo5.anser.value == "くじゃく" || document.fo5.anser.value == "孔雀" || document.fo5.anser.value == "インドクジャク") {
         alert("正解！🐏");
-    }else {
+    } else {
         alert("違います🐎");
     }
 }
